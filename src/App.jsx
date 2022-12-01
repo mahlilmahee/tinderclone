b70dfa524d0f692a8edcb6a65016ecde2da4adde
mahlilmahee <mahlilmahee942@gmail.com>
2022-12-01 14:14:59 +0600

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Dashboard from "./Component/Dashboard"
-import Home from "./Component/Home"
-import Onboarding from "./Component/Onboarding"
+import Home from "./Pages/Home"
+import Dashboard from "./Pages/Dashboard"
+import Onboarding from "./Pages/Onboarding"
+// import Dashboard from "./Component/Dashboard"
+// import Home from "./Component/Home"
+// import Onboarding from "./Component/Onboarding"
 
 
 function App() {
@@ -16,7 +19,7 @@ function App() {
        <Route path="/home" element={<Home></Home>}/>
     
       <Route path="/Dashboard" element={<Dashboard/>}/>
-      <Route path="/onboarding" element={<Onboarding/>}/>
+      <Route path="/onboarding" element={<Onboarding></Onboarding>}/>
      </Routes>
      </BrowserRouter>
     </div>
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from '../images/color-logo-tinder.png';
+import whiteLogo from '../images/tinder_logo_white.png'
+const Navbar = ({minimal,authToken,setModal,setIsSignup}) => {
+ const handleClick=()=>{
+    setModal(true)
+    setIsSignup(false)
+ }
+
+    return (
+        <nav>
+            <div className="logo_container">
+                <img src={minimal? logo :whiteLogo} alt="tinder Logo" className="logo" />
+            </div>
+            {
+                !authToken && !minimal&& <button className="nav-button" onClick={handleClick}>Log In</button>
+            }
+        </nav>
+    );
+};
+
+export default Navbar;
